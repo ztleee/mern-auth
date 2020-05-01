f5347fe75845cd10795f3dc7e87f099ced90a577
zhitao <zhitao.lee.2016@sis.smu.edu.sg>
2020-05-01 17:05:23 +0800

    register and login api endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-require('dotenv').config();
+const users = require("./routes/api/users");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+  );
+  app.use(bodyParser.json());
+  
+require("dotenv").config();
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
+// //passport config
+// app.use(passport.initialize());
+// require("./config/passport")(passport);
+//routes config 
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
